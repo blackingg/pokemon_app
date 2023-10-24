070df7e3d99632d91093be599f53fd3d2f974a74
Mubarak <mudet04@gmail.com>
2023-10-24 13:13:12 +0100

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,53 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
+import { typeColors } from "./setup";
 
 function App() {
 	const [currentList, setCurrentList] = useState([]);
 	const [currentlyShowingAmount, setCurrentlyShowingAmount] = useState(0);
 	const [maxIndex, setMaxIndex] = useState(29);
 
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				// Fetch data from the Pokemon API
-				const response = await fetch(
-					"https://pokeapi.co/api/v2/pokemon/?limit=898"
+	// Function for fetching data and updating the state
+	async function fetchData() {
+		try {
+			// Fetch data from the Pokemon API
+			const response = await fetch(
+				"https://pokeapi.co/api/v2/pokemon/?limit=898"
+			);
+			const data = await response.json();
+
+			const pokemonList = data.results.map((pokemon, index) => ({
+				id: index + 1,
+				name: pokemon.name,
+				types: [],
+			}));
+
+			for (let i = 0; i < 18; i++) {
+				const typeResponse = await fetch(
+					`https://pokeapi.co/api/v2/type/${i + 1}`
 				);
-				const data = await response.json();
+				const typeData = await typeResponse.json();
 
-				const pokemonList = data.results.map((pokemon, index) => {
-					return {
-						id: index + 1,
-						name: pokemon.name,
-						types: [],
-					};
-				});
+				const pokemonInType = typeData.pokemon;
 
-				for (let i = 0; i < 18; i++) {
-					const typeResponse = await fetch(
-						`https://pokeapi.co/api/v2/type/${i + 1}`
-					);
-					const typeData = await typeResponse.json();
-
-					const pokemonInType = typeData.pokemon;
-
-					for (let j = 0; j < pokemonInType.length; j++) {
-						const pokemonId = pokemonInType[j].pokemon.url
-							.split("/")
-							.slice(-2)[0];
-						if (pokemonId <= pokemonList.length) {
-							pokemonList[pokemonId - 1].types.push(typeData.name);
-						}
+				for (let j = 0; j < pokemonInType.length; j++) {
+					const pokemonId = pokemonInType[j].pokemon.url
+						.split("/")
+						.slice(-2)[0];
+					if (pokemonId <= pokemonList.length) {
+						pokemonList[pokemonId - 1].types.push(typeData.name);
 					}
 				}
-
-				setCurrentList(pokemonList);
-				console.log(pokemonList);
-			} catch (error) {
-				console.error("Error fetching data:", error);
 			}
-		}
 
+			setCurrentList(pokemonList);
+			console.log(pokemonList);
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	}
+
+	useEffect(() => {
 		fetchData();
 	}, []);
 
@@ -70,28 +70,36 @@ function App() {
 		};
 	}, [currentList, currentlyShowingAmount]);
 
+	const openInfo = (id) => {
+		// Define openInfo function logic here
+	};
+
 	return (
 		<div className="bg-gray-100 min-h-screen flex justify-center items-center overflow-x-hidden">
 			<div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-screen-xl">
 				<h1 className="text-2xl text-center text-blue-800">Pokemon List</h1>
 				<div className="flex flex-wrap">
-					{currentList.map((_, index) => (
+					{currentList.slice(0, maxIndex).map((pokemon) => (
 						<div
+							key={pokemon.id}
 							className="flex-1 w-1/5 md:w-1/5 lg:w-1/5 m-2 mt-16 pt-10 relative cursor-pointer border-2 border-white transition-transform transform duration-100 hover:scale-105"
-							onClick={() => openInfo(currentList[index].id)}
+							onClick={() => openInfo(pokemon.id)}
 						>
 							<img
-								className=""
-								src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${currentList[index].id}.png`}
+								src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+								alt={pokemon.name}
 							/>
-							<span className="font-size-12 bold">{currentList[index].id}</span>
-
-							<h2 className="text-base font-semibold mt-2">
-								{currentList[index].name}
-							</h2>
-
-							<div className="row center">
-								{currentList[index].types.join(", ")}
+							<span className="text-xl">N° {pokemon.id}</span>
+							<h2 className="text-base font-semibold mt-2">{pokemon.name}</h2>
+							<div className="flex space-x-2">
+								{pokemon.types.map((type, typeIndex) => (
+									<button
+										key={typeIndex}
+										className="px-2 py-1 bg-blue-500 text-white rounded-md"
+									>
+										{type}
+									</button>
+								))}
 							</div>
 						</div>
 					))}
