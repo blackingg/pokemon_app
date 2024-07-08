94b2cfcbf9b0aa9b1600274906f2d4da0617bd7a
blackingg <mudet04@gmail.com>
2024-07-09 00:45:50 +0100

    #

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     try {
       setDataLoading(true);
       const response = await fetch(
-        "https://pokemon-app-backend-9zl6pfrh3-blackinggs-projects.vercel.app/api/pokemon"
+        "https://pokemon-app-backend.vercel.app/api/pokemon"
       );
       const data = await response.json();
 
